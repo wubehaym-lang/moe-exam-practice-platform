830c04ba7b177c7aa86276a99deb8a1b8c75def5
wubehaym-lang <wubehaym@gmail.com>
2026-05-22 17:14:28 -0700

    Refactor Firebase initialization for compatibility

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB04dUF4ULHTlBobhU5Ms4NYB2cANwqrhA",
   authDomain: "moe-exam-platform.firebaseapp.com",
@@ -17,6 +9,5 @@ const firebaseConfig = {
   measurementId: "G-61Y05CXE3T"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
